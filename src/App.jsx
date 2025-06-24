6d03edb0f6236e328329999322e27b657ad812b0
prabathkiran <prabathkiran1@gmail.com>
2025-06-24 17:45:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { ToastContainer, toast } from "react-toastify";
 function App() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
+
+  // useEffect to show all products
   useEffect(() => {
     setLoading(true);
     async function fetchAllProducts() {
@@ -25,13 +29,117 @@ function App() {
     }
     fetchAllProducts();
   }, []);
+
+  // useEffect to fetch Categories
+  useEffect(() => {
+    async function fetchCategories() {
+      const response = await fetch(
+        "https://fakestoreapi.com/products/categories"
+      );
+      const data = await response.json();
+      const list = ["All", ...data];
+      setCategories(list || []);
+    }
+    fetchCategories();
+  }, []);
+
+  // useEffect to show specific category items
+  useEffect(() => {
+    async function fetchProducts() {
+      setLoading(true);
+      let url = "";
+
+      if (!selectedCategory || selectedCategory === "All") {
+        url = "https://fakestoreapi.com/products";
+      } else {
+        url = `https://fakestoreapi.com/products/category/${selectedCategory}`;
+      }
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Can't fetch products from the server.");
+        }
+        const data = await response.json();
+        console.log(data);
+        setProducts(data || []);
+      } catch (e) {
+        toast.error(e.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, [selectedCategory]);
+
+  // useEffect(() => {
+  //   if (!selectedCategory || selectedCategory === "All") {
+  //     setLoading(true);
+  //     async function fetchAllProducts() {
+  //       try {
+  //         const response = await fetch("https://fakestoreapi.com/products");
+  //         if (!response.ok) {
+  //           throw new Error("Can't fetch product with this url");
+  //         }
+  //         const data = await response.json();
+
+  //         setProducts(data || []);
+  //       } catch (e) {
+  //         toast.error(e.message);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     }
+  //     fetchAllProducts();
+  //   } else {
+  //     setLoading(true);
+  //     async function fetchSelectedCategory() {
+  //       try {
+  //         const response = await fetch(
+  //           `https://fakestoreapi.com/products/category/${selectedCategory}`
+  //         );
+  //         if (!response.ok) {
+  //           throw new Error("Can't fetch product with this Category");
+  //         }
+  //         const data = await response.json();
+
+  //         setProducts(data || []);
+  //       } catch (e) {
+  //         toast.error(e.message);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     }
+  //     fetchSelectedCategory();
+  //   }
+  // }, [selectedCategory]);
+
+  function setSelectedCategoryHandler(category) {
+    setSelectedCategory(category);
+  }
   return (
     <>
-      {loading ? (
-        <ClipLoader color="#000" size={50} />
-      ) : (
-        products.map((product) => <div key={product.id}>{product.id}</div>)
-      )}
+      <div id="Container">
+        <div id="Header">
+          <h2>New Collection</h2>
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategoryHandler(e.target.value)}
+          >
+            {categories.map((category) => (
+              <option key={category}>{category}</option>
+            ))}
+          </select>
+        </div>
+        <div id="Products">
+          {loading ? (
+            <ClipLoader color="#000" size={50} />
+          ) : (
+            products.map((product) => <div key={product.id}>{product.id}</div>)
+          )}
+        </div>
+      </div>
       <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
